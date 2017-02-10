d8dfeed083544e464576d5ac4cf9897ade0eb0ad
Cheng Yanan <mrcheng0910@gmail.com>
2017-02-10 13:38:42 +0800

    测试

--- txt_records.py
+++ txt_records.py
@@ -1,4 +1,20 @@
 # encoding:utf-8
 
+import pandas as pd
+import numpy as np
+from pandas import Series,DataFrame
+
+# dd = pd.read_csv('txt.csv',nrows=10)
+#
+# print np.unique(dd['name'])
+
+# chunksize = 10 ** 4
+# d = Series()
+# for chunk in pd.read_csv('txt.csv', chunksize=chunksize):
+#     # print d
+#     d = np.union1d(np.unique(chunk['name']), d)
+#     print d.shape
+#
+# print d
 print "nihao"
-print "你好"
+print "爱国"
